b42f58f51256a611572e81a665d7f080c478ff63
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-24 20:11:07 -0300

    Fix button color selection

--- src/Components/ButtonComponent/index.js
+++ src/Components/ButtonComponent/index.js
@@ -2,8 +2,23 @@ import React from "react";
 
 export const ButtonComponent = ({children, color}) => {
 
-    const mainColor = `bg-${color}-600`;
-    const hoverColor = `hover:bg-${color}-700`;
+    const colors = {
+        'red': {
+            mainColor: 'bg-red-600',
+            hoverColor: 'bg-red-700'
+        },
+        'green': {
+            mainColor: 'bg-green-600',
+            hoverColor: 'bg-green-700'
+        },
+        'blue': {
+            mainColor: 'bg-blue-600',
+            hoverColor: 'bg-blue-700'
+        }
+    }
+
+    const mainColor = colors[color].mainColor;
+    const hoverColor = colors[color].hoverColor;
 
     return(
         <button className={`py-2 px-4 flex justify-center items-center ${hoverColor} ${mainColor}  text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md  w-12 h-12 rounded-lg `}>
