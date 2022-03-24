9f62a3d8ef2f25e762a1c827c2f3c090a1bbd9e6
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-24 13:20:30 -0300

    Reoganize table components

--- src/Components/TableComponent/index.js
+++ src/Components/TableComponent/index.js
@@ -1,40 +1,11 @@
 import React from "react";
-import { ButtonComponent } from "../ButtonComponent";
-import {IoClose} from 'react-icons/io5'
-import {AiOutlineEdit} from 'react-icons/ai'
+import { ReadOnlyRow } from "./ReadOnlyRow";
 
 const tableBackgroundColor = 'bg-white';
 
 const TableHeading = ({ children }) => {
     return (
         <th scope="col" className={`px-5 py-3 ${tableBackgroundColor} border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-bold`}>{children}</th>
-    )
-}
-
-const TableRow = ({ entry }) => {
-
-    return (
-        <tr>
-            {Object.entries(entry).map(e => {
-                return (
-                    <td className={`px-5 py-5 border-b border-gray-200 ${tableBackgroundColor} text-sm`}>
-                        <p className="text-gray-900 whitespace-no-wrap">
-                            {e[1]}
-                        </p>
-                    </td>
-                )
-            })}
-            <td className={`px-5 py-5 border-b border-gray-200 ${tableBackgroundColor} text-sm`}>
-                <ButtonComponent color='blue'>
-                    <AiOutlineEdit/>
-                </ButtonComponent>
-            </td>
-            <td className={`px-5 py-5 border-b border-gray-200 ${tableBackgroundColor} text-sm`}>
-                <ButtonComponent color='red'>
-                    <IoClose size={150}/>
-                </ButtonComponent>
-                </td>
-        </tr>
     )
 }
 
@@ -54,7 +25,7 @@ export const TableComponent = ({ fields, data }) => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {data.map(e => <TableRow entry={e} />)}
+                                    {data.map(e => <ReadOnlyRow entry={e} bgColor={tableBackgroundColor} />)}
                                 </tbody>
                             </table>
                         </div>
