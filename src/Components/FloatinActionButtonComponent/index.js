ee941e31496a1ac44323629112d11b55ff4ba337
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-24 23:07:31 -0300

    Create floating action button

---
+++ src/Components/FloatinActionButtonComponent/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function FloatingActionButtonComponent({ children, color }) {
+
+    const colors = {
+        'red': {
+            mainColor: 'bg-red-600',
+            hoverColor: 'hover:bg-red-700'
+        },
+        'green': {
+            mainColor: 'bg-green-600',
+            hoverColor: 'hover:bg-green-700'
+        },
+        'blue': {
+            mainColor: 'bg-blue-600',
+            hoverColor: 'hover:bg-blue-700'
+        }
+    };
+
+    const mainColor = colors[color].mainColor;
+    const hoverColor = colors[color].hoverColor;
+
+    return (
+        <button type="button" class={`fixed bottom-12 right-12 py-4 px-4  ${mainColor} ${hoverColor} text-white transition ease-in duration-200 text-center text-base w-14 h-14 font-semibold shadow-md rounded-full`}>
+            {children}
+        </button>
+    )
+}
+
+export default FloatingActionButtonComponent;
