62ecc315d4640bde42fcd9b68daa18529a04cc40
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-27 18:08:39 -0300

    Fix typo

--- src/Components/CircularLoadingComponent/index.js
+++ src/Components/CircularLoadingComponent/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const CircularLoading = () => {
     return (
         <div className="flex items-center justify-center w-screen h-screen">
-            <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+            <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
                 <span className="visually-hidden"></span>
             </div>
         </div>
