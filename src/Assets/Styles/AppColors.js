48b4e9be8d278748e7215d32c31ce7cd585a5d8d
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-26 22:35:07 -0300

    Create AppColors file

---
+++ src/Assets/Styles/AppColors.js
@@ -0,0 +1,22 @@
+export const AppColors = {
+    'red': {
+        mainColor: 'bg-red-600',
+        hoverColor: 'hover:bg-red-700'
+    },
+    'green': {
+        mainColor: 'bg-green-600',
+        hoverColor: 'hover:bg-green-700'
+    },
+    'blue': {
+        mainColor: 'bg-blue-600',
+        hoverColor: 'hover:bg-blue-700'
+    },
+    'disabled': {
+        mainColor: 'bg-gray-100',
+        hoverColor: ''
+    },
+    'navigatorButton': {
+        mainColor: 'bg-white',
+        hoverColor: 'hover:bg-gray-100'
+    }
+}
--- src/Components/ButtonComponent/index.js
+++ src/Components/ButtonComponent/index.js
@@ -1,24 +1,10 @@
 import React from "react";
+import { AppColors } from "../../Assets/Styles/AppColors";
 
 export const ButtonComponent = ({children, color}) => {
 
-    const colors = {
-        'red': {
-            mainColor: 'bg-red-600',
-            hoverColor: 'hover:bg-red-700'
-        },
-        'green': {
-            mainColor: 'bg-green-600',
-            hoverColor: 'hover:bg-green-700'
-        },
-        'blue': {
-            mainColor: 'bg-blue-600',
-            hoverColor: 'hover:bg-blue-700'
-        }
-    }
-
-    const mainColor = colors[color].mainColor;
-    const hoverColor = colors[color].hoverColor;
+    const mainColor = AppColors[color].mainColor;
+    const hoverColor = AppColors[color].hoverColor;
 
     return(
         <button className={`py-2 px-4 flex justify-center items-center ${hoverColor} ${mainColor}  text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md  w-12 h-12 rounded-lg `}>
