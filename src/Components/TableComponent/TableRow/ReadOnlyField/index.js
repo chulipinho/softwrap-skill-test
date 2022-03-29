481c5f2e407bd6e91157febffe89394a6390bd4f
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-29 15:41:04 -0300

    Fix typo

--- src/Components/TableComponent/TableRow/ReadOnlyField/index.js
+++ src/Components/TableComponent/TableRow/ReadOnlyField/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ReadOnlyField = ({children, bgColor}) => {
   return (
     <td className={`px-5 py-5 border-b border-gray-200 ${bgColor} text-sm`}>
-    <p className="text-gray-900 whitespace-no-wrap">
+    <p className="text-gray-900 whitespace-nowrap">
         {children}
     </p>
 </td>
