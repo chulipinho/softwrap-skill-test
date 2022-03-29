a229014a4c8c1173191a7ee01b7675fdd72575e5
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-03-29 15:40:33 -0300

    Layout format

--- src/Components/TableComponent/TableHeading/index.js
+++ src/Components/TableComponent/TableHeading/index.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const TableHeading = ({ children, bgColor }) => {
     return (
-        <th scope="col" className={`px-5 py-3 ${bgColor} border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-bold`}>{children}</th>
+        <th scope="col" className={`px-5 py-3 ${bgColor} border-b border-gray-200 text-gray-800 whitespace-nowrap text-left text-sm uppercase font-bold`}>{children}</th>
     )
 }
